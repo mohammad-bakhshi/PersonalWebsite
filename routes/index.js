37c1e494543647aa27edaaeb42f6ab3f6ed786f0
MohammadBakhshi19 <77542557+MohammadBakhshi19@users.noreply.github.com>
2022-04-06 18:50:52 +0430

    add download route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Personal Website' });
+});
+
+app.get('/download', function (req, res) {
+  const file = path.join(__dirname, "../public/data/MohammadBakhshi_Resume.pdf");
+  res.download(file);
 });
 
 module.exports = router;

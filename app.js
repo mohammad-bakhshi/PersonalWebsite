38d6606d39a9bc20f434f67cfadd9c3e0379e15b
Mohammad Bakhshi <77542557+mohammad-bakhshi@users.noreply.github.com>
2023-01-13 19:23:13 +0330

    update

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -35,4 +35,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(3030, () => console.log('Server is listening on port 3030'));

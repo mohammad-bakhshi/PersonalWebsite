bac18ad91b0006a74e7cccf1febec120b019853e
Mohammad Bakhshi <77542557+mohammad-bakhshi@users.noreply.github.com>
2023-02-01 14:42:20 +0330

    add email functionality

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require('dotenv').config();
 
-const indexRouter = require('./routes/index');;
+const indexRouter = require('./routes/index');
+const emailRouter = require('./routes/sendemail');
 
 const app = express();
 
@@ -18,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/email', emailRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
